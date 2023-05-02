03af793825aa6644e5d4551505e697efc42e6d14
Your Name <youremail@yourdomain.com>
2023-05-02 12:06:08 +0300

    cc

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -16,8 +16,7 @@ export default function Home() {
         <div className="widgets block md:flex p-[20px]">
           <Widget type='users' />
           <Widget type='projects' />
-          <Widget type='contractors' />
-         
+          <Widget type='contractors' />        
         </div>
         <div className="listContainer p-[20px] m-[20px]">
           <div className="listTitle font-medium text-neutral-400 mb-3.5">Projects table</div>
