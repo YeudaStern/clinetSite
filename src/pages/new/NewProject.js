953b02eb35bbf5d5a621d7e7dda10257145c62a7
Your Name <youremail@yourdomain.com>
2023-05-03 02:59:28 +0300

    aa

--- src/pages/new/NewProject.js
+++ src/pages/new/NewProject.js
@@ -1,6 +1,8 @@
 import React from "react"
 import BarSide from "../../components/barside/BarSide"
 import BarNav from "../../components/navbar/BarNav"
+import { Form } from "react-router-dom"
+import { Label } from "@mui/icons-material"
 
 
 export default function NewProject() {
@@ -9,10 +11,11 @@ export default function NewProject() {
       <BarSide />
       <div className="flex-[10]">
         <BarNav />
-        <div className="'p-[20px] m-[20px]"> <div className='font-medium text-neutral-400 mb-3.5  p-[8px] shadow  '>
-          <div className='basis-1/2'> Add new project</div>
+        <div className="'p-[20px] m-[20px]">
+          <div className='font-medium text-neutral-400 p-[8px] shadow'>
+            <div> Add new project</div>
 
-         </div>
+          </div>
         </div>
       </div>
     </div>
