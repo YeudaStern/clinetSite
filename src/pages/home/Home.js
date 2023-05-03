f8214ea64cb12a6575ef9823670aa7aa64676644
Yeuda Shtern <yeudashtern@gmail.com>
2023-05-03 17:15:25 +0300

    ee

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -23,7 +23,7 @@ export default function Home() {
 
   return (
 
-    
+
     <div className="home">
       <BarSide />
       <div className="homeContainer flex-[10]">
@@ -33,30 +33,33 @@ export default function Home() {
           <Widget type='projects' />
           <Widget type='contractors' />
 
-    
-      {/* <ThemeProvider theme={darkThme}></ThemeProvider>
+
+          {/* <ThemeProvider theme={darkThme}></ThemeProvider>
    <IconButton onClick={() => setDark(!dark)}> 
           {dark ? <Brightness7 /> : <Brightness4 />} */}
-      <div className="home flex">
+          <div className="home flex">
 
 
-        <BarSide />
-        <div className="homeContainer flex-[10]">
-          <BarNav />
-          <div className="widgets block md:flex p-[20px]">
-            <Widget type='users' />
-            <Widget type='projects' />
-            <Widget type='contractors' />
+            <BarSide />
+            <div className="homeContainer flex-[10]">
+              <BarNav />
+              <div className="widgets block md:flex p-[20px]">
+                <Widget type='users' />
+                <Widget type='projects' />
+                <Widget type='contractors' />
 
+              </div>
+              <div className="listContainer p-[20px] m-[20px]">
+                <div className="listTitle font-medium text-neutral-400 mb-3.5">Projects table</div>
+                <List type='projects' className='prolist' />
+              </div>
+
+            </div>
           </div>
-          <div className="listContainer p-[20px] m-[20px]">
-            <div className="listTitle font-medium text-neutral-400 mb-3.5">Projects table</div>
-            <List type='projects' className='prolist' />
-          </div>
-
         </div>
       </div>
-      {/* </IconButton>     */}
+    </div>
+
 
   )
 }
--- src/pages/list/List.js
+++ src/pages/list/List.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import React, { useState, useEffect } from 'react';
 import { apiDelete, apiGet } from '../../services/apiServices'
 import { useNavigate, useSearchParams } from 'react-router-dom';
@@ -6,17 +6,9 @@ import { TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Table
 import DeleteIcon from '@mui/icons-material/Delete';
 import EditIcon from '@mui/icons-material/Edit';
 import { toast } from 'react-toastify';
-import { MAIN_ROUTE } from '../../constant/url';
+import { API_URL, MAIN_ROUTE } from '../../constant/url';
 import Box from '@mui/material/Box';
 
-export default function List() {
-
-  const [data, setData] = useState([]);
-  const [query] = useSearchParams();
-  const nav = useNavigate();
-
-  useEffect(() => {
-=======
 
 // import React, { useState, useEffect } from 'react';
 // import axios from 'axios';
@@ -83,41 +75,25 @@ export default function List() {
 // export default ResponsiveTable;
 
 
-
-
-
-
-import React, { useState, useEffect } from 'react';
-import { apiDelete, apiGet } from '../../services/apiServices'
-import { useNavigate, useSearchParams } from 'react-router-dom';
-import { TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Table, Button, Box, Typography } from "@mui/material";
-import DeleteIcon from '@mui/icons-material/Delete';
-import EditIcon from '@mui/icons-material/Edit';
-import { toast } from 'react-toastify';
-import { API_URL, MAIN_ROUTE } from '../../constant/url';
-import { DataGrid } from '@mui/x-data-grid';
-
-
 export default function List() {
- 
-  
+
+
   const [data, setData] = useState([]);
   const [query] = useSearchParams();
   const nav = useNavigate();
-  
+
   useEffect(() => {
-   
->>>>>>> table_of-_project
+
+
     doApi();
   }, [query])
 
 
   const doApi = async () => {
-<<<<<<< HEAD
-    const url = MAIN_ROUTE + 'projects/projectsList';
-=======
+
+
     const url = API_URL + 'projects/projectsList';
->>>>>>> table_of-_project
+
     try {
       const data = await apiGet(url);
       console.log(data);
@@ -131,11 +107,11 @@ export default function List() {
   const deleteProject = async (_idDel) => {
     if (window.confirm("Delete project?")) {
       try {
-<<<<<<< HEAD
-        const url = MAIN_ROUTE + "projects/" + _idDel;
-=======
+
+
+
         const url = API_URL + "projects/" + _idDel;
->>>>>>> table_of-_project
+
         const data = await apiDelete(url, "DELETE");
         if (data.deletedCount) {
           doApi();
@@ -149,74 +125,53 @@ export default function List() {
     }
   }
 
-<<<<<<< HEAD
-  const datad = ()=>{
-    return data;
-  }
 
-  const rows = {
-    datad 
-  }
+
 
   const page = query.get("page") || 1;
   return (
-<Box sx={{width:"100%",  maxHeight: "300px"}}>
-    <TableContainer component={Paper} className='drop-shadow-xl bg-slate-100 '>
-=======
-   const page = query.get("page") || 1;
-
-
-
-   return (
-
   
+      <TableContainer component={Paper} className='drop-shadow-xl bg-slate-100'>
 
-    <TableContainer component={Paper} className='drop-shadow-xl bg-slate-100'>
->>>>>>> table_of-_project
-      <Table className='table-fixed'>
-        <TableHead>
-          <TableRow align="center">
-            <TableCell>Id</TableCell>
-            <TableCell>Project Name</TableCell>
-            <TableCell>City</TableCell>
-            <TableCell>Street</TableCell>
-            <TableCell>Building name(or Number)</TableCell>
-            <TableCell>Contractor Name</TableCell>
-            <TableCell>Dtae created</TableCell>
-            <TableCell>Delete</TableCell>
-            <TableCell>Edit</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody >
-          {data.map((row, i) => (
-            <TableRow align="center" key={row._id} sx={{ '&:last-child td, &:last-child th': { border: 0 } }} >
-              <TableCell>{((page - 1) * 15) + i + 1}</TableCell>
-              <TableCell>{row.p_name}</TableCell>
-              <TableCell>{row.city_name}</TableCell>
-              <TableCell>{row.street_name}</TableCell>
-              <TableCell>{row.building_name}</TableCell>
-              <TableCell>{row.contractor_name}</TableCell>
-              <TableCell>{row.date_created.substring(0, 10)}</TableCell>
-              <TableCell ><Button onClick={() => {
+        <Table className='table-fixed'>
+          <TableHead>
+            <TableRow align="center">
+              <TableCell>Id</TableCell>
+              <TableCell>Project Name</TableCell>
+              <TableCell>City</TableCell>
+              <TableCell>Street</TableCell>
+              <TableCell>Building name(or Number)</TableCell>
+              <TableCell>Contractor Name</TableCell>
+              <TableCell>Dtae created</TableCell>
+              <TableCell>Delete</TableCell>
+              <TableCell>Edit</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody >
+            {data.map((row, i) => (
+              <TableRow align="center" key={row._id} sx={{ '&:last-child td, &:last-child th': { border: 0 } }} >
+                <TableCell>{((page - 1) * 15) + i + 1}</TableCell>
+                <TableCell>{row.p_name}</TableCell>
+                <TableCell>{row.city_name}</TableCell>
+                <TableCell>{row.street_name}</TableCell>
+                <TableCell>{row.building_name}</TableCell>
+                <TableCell>{row.contractor_name}</TableCell>
+                <TableCell>{row.date_created.substring(0, 10)}</TableCell>
+                <TableCell ><Button onClick={() => {
                   deleteProject(row._id);
                 }} className='text-center me-10' size='small' color='error' variant="outlined" startIcon={<DeleteIcon />}>Delete</Button></TableCell>
-              <TableCell><Button size='small' variant="outlined" startIcon={<EditIcon />}>Edit</Button></TableCell>
-<<<<<<< HEAD
-                
-              
-=======
+                <TableCell><Button size='small' variant="outlined" startIcon={<EditIcon />}>Edit</Button></TableCell>
 
-             
->>>>>>> table_of-_project
-            </TableRow>
-          ))
-          }
-        </TableBody>
-      </Table>
-    </TableContainer>
-<<<<<<< HEAD
-    </Box>
-=======
->>>>>>> table_of-_project
-  )
-}
+
+              </TableRow>
+            ))
+            }
+          </TableBody>
+        </Table>
+      </TableContainer>
+
+    
+
+
+      )
+    }
