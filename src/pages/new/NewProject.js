e5c00f49a3446e37b9d9201da074a5b526a52707
Your Name <youremail@yourdomain.com>
2023-05-03 17:28:57 +0300

    save

--- src/pages/new/NewProject.js
+++ src/pages/new/NewProject.js
@@ -1,8 +1,7 @@
 import React from "react"
 import BarSide from "../../components/barside/BarSide"
 import BarNav from "../../components/navbar/BarNav"
-import { Form } from "react-router-dom"
-import { Label } from "@mui/icons-material"
+
 
 
 export default function NewProject() {
