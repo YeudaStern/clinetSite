abbb184da200250f905ab115a85094a24edee86b
Your Name <youremail@yourdomain.com>
2023-05-05 01:05:42 +0300

    save

--- src/components/navbar/BarNav.js
+++ src/components/navbar/BarNav.js
@@ -1,7 +1,6 @@
 
 
 import * as React from 'react';
-import FullscreenExitOutlinedIcon from '@mui/icons-material/FullscreenExitOutlined';
 import MailIcon from '@mui/icons-material/Mail';
 // k
 import IconButton from '@mui/material/IconButton';
--- src/pages/lists/UsersList.js
+++ src/pages/lists/UsersList.js
@@ -1,9 +1,9 @@
-import { DataGrid } from '@mui/x-data-grid';
-import {  Button, Box } from "@mui/material";
+import { DataGrid, GridToolbar } from '@mui/x-data-grid';
+import { Button, Box } from "@mui/material";
 import { API_URL } from '../../constant/url';
 import React, { useState, useEffect } from 'react';
 import BarNav from '../../components/navbar/BarNav';
-import {  apiDelete, apiGet } from '../../services/apiServices';
+import { apiDelete, apiGet } from '../../services/apiServices';
 import BarSide from '../../components/barside/BarSide';
 import { Link, useSearchParams } from 'react-router-dom';
 import { toast } from 'react-toastify';
@@ -98,34 +98,35 @@ export default function UsersList() {
 
 
   return (
-    <div className='flex'>
-      <BarSide />
-      <div className='flex-[10]'>
-        <BarNav />
-        <div className='p-[20px] md:m-[20px] md:w-auto w-screen'>
-          <div className='font-medium text-neutral-400 mb-0.5 border-2 p-[8px]  flex justify-between'>
-            Users Table
-            <Button size="small" variant="contained" className='items-end' >
-              <Link to='/users/newUser' className='hover:text-white'>Add new user</Link>
-            </Button> 
-          </div>
-          <Box sx={{ height: 400, width: '100%'}}>
-            <DataGrid
-              rows={data}
-              columns={columns}
-              initialState={{
-                pagination: {
-                  paginationModel: {
-                    pageSize: 5,                 
-                  },
-                },
-              }}
-              pageSizeOptions={[5]}
-              checkboxSelection
-              disableRowSelectionOnClick
-            />
-         
-          </Box>
-        </div>
+
+    <div className='p-[20px] md:m-[20px] md:w-auto w-screen'>
+      <div className='font-medium text-neutral-400 mb-0.5 border-2 p-[8px]  flex justify-between'>
+        Users Table
+        <Button size="small" variant="contained" className='items-end' >
+          <Link to='/users/newUser' className='hover:text-white'>Add new user</Link>
+        </Button>
+      </div>
+      <Box sx={{ height: 400, width: '100%' }}>
+        <DataGrid
+          rows={data}
+          columns={columns}
+          editMode='row'
+          slots={{
+            toolbar: GridToolbar,
+          }}
+          initialState={{
+            pagination: {
+              paginationModel: {
+                pageSize: 5,
+              },
+            },
+          }}
+          pageSizeOptions={[5]}
+          checkboxSelection
+          disableRowSelectionOnClick
+        />
+
+      </Box>
+    </div>
   )
 }
